5da9cf1ddb57e54f24dcc9765ccbd26a5974eac2
Raül Pérez <repejota@gmail.com>
2018-02-08 14:44:52 +0100

    example test

--- example_test.go
+++ example_test.go
@@ -15,132 +15,16 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-package cscanner
+package cscanner_test
 
 import (
-	"bufio"
-	"context"
 	"fmt"
 	"io"
 	"os"
-	"sync"
 	"time"
+
+	"github.com/repejota/cscanner"
 )
-
-func main() {
-	// Delay reader writes data of lengths, each delayed by wait
-	a := &delayReader{chars: "a", lengths: []int{10, 45, 3}, wait: 1 * time.Second}
-	b := &delayReader{chars: "b", lengths: []int{3, 100, 23, 12, 57, 34}, wait: 500 * time.Millisecond}
-
-	fmt.Println("MultiReader:")
-	multi := io.MultiReader(a, b)
-	io.Copy(os.Stdout, multi)
-
-	fmt.Println()
-
-	// Create new, unread, readers
-	a = &delayReader{chars: "a", lengths: []int{10, 45, 3}, wait: 1 * time.Second}
-	b = &delayReader{chars: "b", lengths: []int{3, 100, 23, 12, 57, 34}, wait: 500 * time.Millisecond}
-
-	fmt.Println("ConcurrentScanner:")
-	scanner := NewConcurrentScanner(a, b)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error: ", err)
-	}
-}
-
-// ConcurrentScanner works like io.Scanner, but with multiple io.Readers
-type ConcurrentScanner struct {
-	scans  chan []byte   // Scanned data from readers
-	errors chan error    // Errors from readers
-	done   chan struct{} // Signal that all readers have completed
-	cancel func()        // Cancel all readers (stop on first error)
-
-	data []byte // Last scanned value
-	err  error
-}
-
-// NewConcurrentScanner starts scanning each reader in a separate goroutine
-// and returns a *ConcurrentScanner.
-func NewConcurrentScanner(readers ...io.Reader) *ConcurrentScanner {
-	ctx, cancel := context.WithCancel(context.Background())
-	s := &ConcurrentScanner{
-		scans:  make(chan []byte),
-		errors: make(chan error),
-		done:   make(chan struct{}),
-		cancel: cancel,
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(readers))
-
-	for _, reader := range readers {
-		// Start a scanner for each reader in it's own goroutine.
-		go func(reader io.Reader) {
-			defer wg.Done()
-			scanner := bufio.NewScanner(reader)
-
-			for scanner.Scan() {
-				select {
-				case s.scans <- scanner.Bytes():
-					// While there is data, send it to s.scans,
-					// this will block until Scan() is called.
-				case <-ctx.Done():
-					// This fires when context is cancelled,
-					// indicating that we should exit now.
-					return
-				}
-			}
-			if err := scanner.Err(); err != nil {
-				select {
-				case s.errors <- err:
-					// Reprort we got an error
-				case <-ctx.Done():
-					// Exit now if context was cancelled, otherwise sending
-					// the error and this goroutine will never exit.
-					return
-				}
-			}
-		}(reader)
-	}
-
-	go func() {
-		// Signal that all scanners have completed
-		wg.Wait()
-		close(s.done)
-	}()
-
-	return s
-}
-
-func (s *ConcurrentScanner) Scan() bool {
-	select {
-	case s.data = <-s.scans:
-		// Got data from a scanner
-		return true
-	case <-s.done:
-		// All scanners are done, nothing to do.
-	case s.err = <-s.errors:
-		// One of the scanners error'd, were done.
-	}
-	s.cancel() // Cancel context regardless of how we exited.
-	return false
-}
-
-func (s *ConcurrentScanner) Bytes() []byte {
-	return s.data
-}
-
-func (s *ConcurrentScanner) Text() string {
-	return string(s.data)
-}
-
-func (s *ConcurrentScanner) Err() error {
-	return s.err
-}
 
 type delayReader struct {
 	chars   string
@@ -166,4 +50,30 @@ func (r *delayReader) Read(b []byte) (int, error) {
 	i++
 	time.Sleep(r.wait)
 	return i, nil
+}
+
+func ExampleConcurrentScanner() {
+	// Delay reader writes data of lengths, each delayed by wait
+	a := &delayReader{chars: "a", lengths: []int{10, 45, 3}, wait: 1 * time.Second}
+	b := &delayReader{chars: "b", lengths: []int{3, 100, 23, 12, 57, 34}, wait: 500 * time.Millisecond}
+
+	fmt.Println("MultiReader:")
+	multi := io.MultiReader(a, b)
+	io.Copy(os.Stdout, multi)
+
+	fmt.Println()
+
+	// Create new, unread, readers
+	a = &delayReader{chars: "a", lengths: []int{10, 45, 3}, wait: 1 * time.Second}
+	b = &delayReader{chars: "b", lengths: []int{3, 100, 23, 12, 57, 34}, wait: 500 * time.Millisecond}
+
+	fmt.Println("ConcurrentScanner:")
+	readers := []io.Reader{a, b}
+	scanner := cscanner.NewConcurrentScanner(readers)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
